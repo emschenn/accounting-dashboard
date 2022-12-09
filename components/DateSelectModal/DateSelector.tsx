fccbcaf715d844d09a9c07fb91112906817007be
emschenn <emilynov14@gmail.com>
2022-12-10 00:30:36 +0800

    :lipstick: update style of date

--- components/DateSelectModal/DateSelector.tsx
+++ components/DateSelectModal/DateSelector.tsx
@@ -5,29 +5,50 @@ type Props = {
   openModal: () => void;
 };
 
+type DateTimeFormatOptions = {
+  weekday?: "long" | "short" | "narrow";
+  year?: "numeric" | "2-digit";
+  month?: "numeric" | "2-digit" | "long" | "short" | "narrow";
+  day?: "numeric" | "2-digit";
+};
+
 function DateSelector({ selectedDateRange, openModal }: Props) {
   const [shownDate, setShownDate] = useState<string>("");
+  const [dateStyle, setDateStyle] = useState<string>("");
+
   useEffect(() => {
     const { start, end } = selectedDateRange;
     if (start.slice(0, 7) === end.slice(0, 7)) {
-      let year = +start.slice(0, 4);
-      let month = +start.slice(5, 7);
-      setShownDate(`${month} ${year}`);
+      const option: DateTimeFormatOptions = {
+        year: "numeric",
+        month: "short",
+      };
+      new Date(start).toLocaleDateString("en-US", option);
+      const date = new Date(start).toLocaleDateString("en-US", option);
+      setShownDate(date.replace(" ", ", "));
+      setDateStyle("text-4xl");
     } else {
-      setShownDate(`${start}-${end}`);
+      const option: DateTimeFormatOptions = {
+        year: "numeric",
+        month: "short",
+        day: "numeric",
+      };
+      let startDate = new Date(start).toLocaleDateString("en-US", option);
+      let endDate = new Date(end).toLocaleDateString("en-US", option);
+      if (endDate.slice(-4) === startDate.slice(-4)) {
+        startDate = startDate.slice(0, -6);
+      }
+      setShownDate(`${startDate} 〰️ ${endDate}`);
+      setDateStyle("text-2xl");
     }
   }, [selectedDateRange]);
 
   return (
     <div
-      className="pb-6 text-5xl font-bold text-neutral-700"
+      className={`pb-6 font-bold text-neutral-700 ${dateStyle}`}
       onClick={openModal}
     >
       {shownDate}
-      {/* {new Date().toLocaleString("default", {
-        month: "short",
-      })} */}
-      {/* <span className=" text-splitwise"> {new Date().getFullYear()}</span> */}
     </div>
   );
 }
