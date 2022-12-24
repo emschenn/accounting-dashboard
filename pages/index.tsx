e7073e5402e0c9d39d10d398510ed48a41eb69e9
emschenn <emilynov@14@gmail.com>
2022-12-24 13:18:52 +0800

    :lipstick: change favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,7 +37,7 @@ export default function Home() {
           href="https://api.fontshare.com/v2/css?f[]=satoshi@900,700,500,301,701,300,501,401,901,400&display=swap"
           rel="stylesheet"
         ></link>
-        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/favicon.png" />
         <title>$$$</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
