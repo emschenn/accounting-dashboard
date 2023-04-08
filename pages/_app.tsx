3826e23cbb869bab9924328aaf6f95be0fad8ed4
emschenn <emilynov14@gmail.com>
2023-04-08 16:16:07 +0800

    :rocket: add pwa menifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,12 +8,6 @@ import { SplitwiseContextProvider } from "../contexts/splitwiseContext";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <link rel="apple-touch-icon" href="/app-icon.png" />
-        <link rel="icon" type="image/x-icon" href="/web-icon.png" />
-        <title>( ❛ ͜ʖ ❛ )</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
       <SplitwiseContextProvider>
         <Component {...pageProps} />
       </SplitwiseContextProvider>
--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="manifest" href="/manifest.json" />
         <link
           href="https://api.fontshare.com/v2/css?f[]=satoshi@900,700,600,500,301,701,300,501,401,901,400&display=swap"
           rel="stylesheet"
@@ -12,6 +13,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Krona+One&display=swap"
           rel="stylesheet"
         />
+        <link rel="apple-touch-icon" href="/app-icon.png" />
+        <link rel="icon" type="image/x-icon" href="/web-icon.png" />
+        {/* <title>( ❛ ͜ʖ ❛ )</title> */}
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <body className="bg-custom-bg">
         <Main />
